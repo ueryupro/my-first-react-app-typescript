b7095fceb08f833dfae0c4a1b7ada04ba55ddd2d
ueryu <nittymahi@gmail.com>
2021-07-13 10:44:59 +0900

    first

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App message="Hello, React!" />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
